ca08f8e3eb0ba0374b00d6c5ac810cc1ce0b0eb5
MaelVouillon <maelpitch@gmail.com>
2024-11-28 23:52:56 +0100

    ajout de la page de connexion front et début du back

--- my-platform-backend/middleware/authMiddleware.js
+++ my-platform-backend/middleware/authMiddleware.js
@@ -2,17 +2,17 @@
 
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1]; // Récupère le token de l'en-tête Authorization
-  if (!token) {
-    return res.status(401).json({ error: 'Accès non autorisé.' });
-  }
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'Accès refusé. Token manquant.' });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Vérifie le token avec la clé secrète
-    req.user = decoded; // Ajoute les informations de l'utilisateur à la requête
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
     next();
-  } catch (error) {
-    return res.status(401).json({ error: 'Token invalide.' });
+  } catch {
+    return res.status(400).json({ error: 'Token invalide.' });
   }
 };
+
+module.exports = authMiddleware;
